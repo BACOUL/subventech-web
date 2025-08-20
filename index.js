1d3bf8c3d466c91ad7fd2d9a65c1fd6f640c1d29
BACOUL <102251510+BACOUL@users.noreply.github.com>
2025-08-20 23:01:03 +0200

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: "center", padding: "50px" }}>
-      <h1>Bienvenue sur SubvenTech 🚀</h1>
-      <p>Votre assistant pour obtenir et gérer vos subventions plus facilement.</p>
-      <a href="/brand">Voir la charte graphique</a>
-    </div>
-  )
-}
